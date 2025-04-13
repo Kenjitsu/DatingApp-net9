c506da5bd074f10bc511ec39d3788c657ff2d23b
Ricardo Corredor <ricorga_97@hotmail.com>
2025-04-13 00:12:58 -0500

    Incialización del proyecto - Final sección 2.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Add any custom model configurations here
+    }
+}
