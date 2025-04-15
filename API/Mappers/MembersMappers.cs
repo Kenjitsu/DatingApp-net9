778d2aab77af08cd2ec89b3ed54b3972c46ba065
Ricardo Corredor <ricorga_97@hotmail.com>
2025-04-15 16:19:53 -0500

    Cleaning membersmappers file

--- API/Mappers/MembersMappers.cs
+++ API/Mappers/MembersMappers.cs
@@ -7,46 +7,10 @@ namespace API.Mappers;
 
 public static class MembersMappers
 {
-    public static IQueryable<MemberDto> ProjectAppUserListToMemberListDtos(this IQueryable<AppUser> userList)
-    {
-        var membersListDto = userList.Select(user => new MemberDto()
-        {
-            Id = user.Id,
-            Username = user.UserName,
-            Age = user.DateOfBirth.CalculateAge(),
-            PhotoUrl = user.Photos == null
-                ? null
-                : user.Photos
-                .Where(p => p.IsMain)
-                .Select(p => p.Url)
-                .FirstOrDefault(),
-            KnownAs = user.KnownAs,
-            CreatedAt = user.CreatedAt,
-            LastActive = user.LastActive,
-            Gender = user.Gender,
-            Introduction = user.Introduction,
-            Interests = user.Interests,
-            LookingFor = user.LookingFor,
-            City = user.City,
-            Country = user.Country,
-            Photos = user.Photos == null
-                ? new List<PhotoDto>()
-                : user.Photos.Select(photo => new PhotoDto
-                {
-                    Id = photo.Id,
-                    IsMain = photo.IsMain,
-                    Url = photo.Url
-                }).ToList()
-            });
-
-        return membersListDto;
-    }
-
     public static IQueryable<MemberDto> ProjectToMemberDtos(this IQueryable<AppUser> users)
     {
         return users.Select(GetMemberDtoProjection());
     }
-
 
     public static Expression<Func<AppUser, MemberDto>> GetMemberDtoProjection()
     {
@@ -80,16 +44,4 @@ public static class MembersMappers
             }).ToList()
         };
     }
-
-    
-
-    //private static string? GetPhotoUrl(IEnumerable<Photo> photosList)
-    //{
-    //    if (photosList != null && photosList.Any())
-    //    {
-    //        return photosList.FirstOrDefault(x => x.IsMain)!.Url ?? null;
-    //    }
-
-    //    return null;
-    //}
 }
