44a2321e6e02659415a8a4f8901dba32d481ff6e
Ricardo Corredor <ricorga_97@hotmail.com>
2026-04-26 22:52:50 -0500

    added delete photo funcionality in the client

--- client/src/core/services/members.service.ts
+++ client/src/core/services/members.service.ts
@@ -46,7 +46,11 @@ export class MembersService {
   }
 
   setMainPhoto(photo: Photo) {
-    return this.http.put(this.baseUrl + 'members/set-main-photo/' + photo.id, {})
+    return this.http.put(this.baseUrl + 'members/set-main-photo/' + photo.id, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'members/delete-photo/' + photoId);
   }
 
   // getMembers() {
@@ -64,16 +68,5 @@ export class MembersService {
 
   
 
-  // deletePhoto(photo: Photo) {
-  //   return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
-  //     tap(() => {
-  //       this.members.update(members => members.map(m => {
-  //         if (m.photos.includes(photo)) {
-  //           m.photos = m.photos.filter(x => x.id !== photo.id)
-  //         }
-  //         return m;
-  //       }))
-  //     })
-  //   );
-  // }
+  
 }
