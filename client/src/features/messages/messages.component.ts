b9409f114497f693888f7106f0696d8191232109
Ricardo Corredor <ricorga_97@hotmail.com>
2026-05-04 00:34:23 -0500

    added delete message feature to the chat in the client

--- client/src/features/messages/messages.component.ts
+++ client/src/features/messages/messages.component.ts
@@ -37,6 +37,37 @@ export class MessagesComponent implements OnInit {
       }
 		});
   }
+
+  deleteMessage(event: Event, id: string) {
+    event.stopPropagation();
+
+    this.messageService.deleteMessage(id).subscribe({
+      next: () => {
+        const current = this.paginatedMessages();
+        if (current?.items) {
+          this.paginatedMessages.update((prev) => {
+            if (!prev) return null;
+            const newItems = prev.items?.filter(x => x.id !== id) || [];
+            const newMetadata = {
+              ...prev.metadata,
+              totalCount: prev.metadata.totalCount - 1,
+              totalPages: Math.max(1, Math.ceil((prev.metadata.totalCount - 1) /
+                prev.metadata.pageSize)),
+              currentPage: Math.min(
+                prev.metadata.currentPage,
+                Math.max(1, Math.ceil((prev.metadata.totalCount - 1) /
+                  prev.metadata.pageSize))
+              )
+            };
+            return {
+              items: newItems,
+              metadata: newMetadata
+            };
+          });
+        }
+      }
+    });
+  }
   
   get isInbox() {
     return this.fetchedContainer === 'Inbox';
