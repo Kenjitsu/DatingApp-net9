01ef222db4ad15e1bcd85c04e840349dfc8ac853
Ricardo Corredor <ricorga_97@hotmail.com>
2026-05-05 03:15:41 -0500

    added estatis files middleware

--- API/Program.cs
+++ API/Program.cs
@@ -35,6 +35,9 @@ app.UseCors(x => x
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/messages");
