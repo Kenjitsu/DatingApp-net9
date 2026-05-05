2a543d7ca2557ce1b835f80180594af57d27eb5f
Ricardo Corredor <ricorga_97@hotmail.com>
2026-05-04 19:37:10 -0500

    added live chat functionality in the client

--- client/src/features/members/member-messages/member-messages.ts
+++ client/src/features/members/member-messages/member-messages.ts
@@ -6,6 +6,7 @@ import { DatePipe } from '@angular/common';
 import { TimeAgoPipe } from '../../../core/pipes/time-ago.pipe';
 import { FormsModule } from '@angular/forms';
 import { PresenceService } from '../../../core/services/presence-service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-messages',
@@ -15,15 +16,15 @@ import { PresenceService } from '../../../core/services/presence-service';
 })
 export class MemberMessages implements OnInit {
   @ViewChild('messageEndRef') messageEndRef!: ElementRef
-  private messageService = inject(MessageService);
+  protected messageService = inject(MessageService);
   private memberService = inject(MembersService);
   protected presenceService = inject(PresenceService);
-  protected messages = signal<Message[]>([]);
+  private route = inject(ActivatedRoute);
   protected messageContent = '';
 
   constructor() {
     effect(() => {
-      const currentMessages = this.messages();
+      const currentMessages = this.messageService.messageThread();
 
       if (currentMessages.length > 0) {
         this.scrollToBottom();  
@@ -32,20 +33,14 @@ export class MemberMessages implements OnInit {
   }
 
   ngOnInit(): void {
-    this.loadMessages();
-  }
+    this.route.parent?.paramMap.subscribe({
+      next: params => {
+        const otherUserId = params.get('id');
+        if (!otherUserId) throw Error('Cannot connect to hub');
+        this.messageService.createHubConnection(otherUserId);
 
-  loadMessages() {
-    const memberId = this.memberService.member()?.id;
-
-    if (memberId) {
-      this.messageService.getMessageThread(memberId).subscribe({
-        next: messages => this.messages.set(messages.map(message => ({
-          ...message,
-          currentUserSender: message.senderId !== memberId
-        })))
-      });
-    }
+      }
+    })
   }
 
   sendMessage() {
@@ -53,17 +48,9 @@ export class MemberMessages implements OnInit {
 
     if (!recipientId) return;
 
-    this.messageService.sendMessage(recipientId, this.messageContent).subscribe({
-      next: message => {
-        this.messages.update(messages => {
-          message.currentUserSender = true;
-
-          return [ ...messages, message ]
-        });
-
-        this.messageContent = '';
-      }
-    });
+    this.messageService.sendMessage(recipientId, this.messageContent)?.then(() => {
+      this.messageContent = '';
+    })
   }
 
   scrollToBottom() {
